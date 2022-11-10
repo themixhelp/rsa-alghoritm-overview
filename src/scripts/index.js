e6f44420ba98b4f44743d26945ec71ca76846180
themixhelp <themixhelp@outlook.com>
2022-11-11 00:31:21 +0100

    initialize keys in index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,23 @@
 import { scrollToNextSlide } from './utilities/scrollToNextSlide.js'
 import { decryptionTime } from './utilities/decryptionTime.js'
+import { cryptingHook } from './utilities/cryptingHook.js'
 
 const SCROLLERS = document.querySelectorAll('.scroller')
+
+// initialize EventListeners on scroller's
+
 SCROLLERS.forEach((scroller) => scroller.addEventListener('click', scrollToNextSlide))
+
+// update percent of 128-key decryption
 
 setInterval(() => {
 	decryptionTime.setTimesDiff()
 }, 1000)
+
+// managing RSA keys
+
+cryptingHook.initializeKeys()
+
+// change scrollRestoration
 
 history.scrollRestoration = 'manual'
