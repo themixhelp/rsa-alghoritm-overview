cf6686ee197465a451d55a150ac8da2f37d64efe
themixhelp <themixhelp@outlook.com>
2022-11-11 00:52:20 +0100

    finish work with crypting hook, change key_size and beautify all files

--- src/scripts/utilities/cryptingHook.js
+++ src/scripts/utilities/cryptingHook.js
@@ -1,16 +1,28 @@
 import { JSEncrypt } from 'jsencrypt'
 
 const cryptingHook = {
-	JSE: new JSEncrypt({ default_key_size: 128 }),
+	JSE: new JSEncrypt({ default_key_size: 1024 }),
 
 	PUBLIC_KEY: null,
 	PRIVATE_KEY: null,
+
+	ENCRYPTED_TEXT: null,
+	DECRYPTED_TEXT: null,
 
 	generatePublicKey() {
 		this.PUBLIC_KEY = this.JSE.getPublicKey()
 	},
 	generatePrivateKey() {
 		this.PRIVATE_KEY = this.JSE.getPrivateKey()
+	},
+
+	encryptText(text) {
+		this.JSE.setPublicKey(this.PUBLIC_KEY)
+		this.ENCRYPTED_TEXT = this.JSE.encrypt(text)
+	},
+	decryptText(text) {
+		this.JSE.setPrivateKey(this.PRIVATE_KEY)
+		this.DECRYPTED_TEXT = this.JSE.decrypt(text)
 	},
 
 	initializeKeys() {
