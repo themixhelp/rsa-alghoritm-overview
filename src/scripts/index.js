da5dc36090c31531e7ac49ad499fb57e72d70935
themixhelp <themixhelp@outlook.com>
2022-11-11 01:12:43 +0100

    generating both keys on EL

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,11 @@ import { cryptingHook } from './utilities/cryptingHook.js'
 
 const SCROLLERS = document.querySelectorAll('.scroller')
 
+const PUBLIC_KEY = document.querySelector('.public-key')
+const PRIVATE_KEY = document.querySelector('.private-key')
+const GENERATE_PUBLIC = document.querySelector('.generate-public')
+const GENERATE_PRIVATE = document.querySelector('.generate-private')
+
 // initialize EventListeners on scroller's
 
 SCROLLERS.forEach((scroller) => scroller.addEventListener('click', scrollToNextSlide))
@@ -17,6 +22,12 @@ setInterval(() => {
 // managing RSA keys
 
 cryptingHook.initializeKeys()
+GENERATE_PUBLIC.addEventListener('click', () => {
+	PUBLIC_KEY.innerText = cryptingHook.PUBLIC_KEY
+})
+GENERATE_PRIVATE.addEventListener('click', () => {
+	PRIVATE_KEY.innerText = cryptingHook.PRIVATE_KEY
+})
 
 // change scrollRestoration
 
